4ca2918f52c8f5434dc49cb3caecf00a8c5f8eea
volv <steven@bigham.com>
2016-01-23 01:28:48 +0000

    Mongodb find project

---
+++ fccfindproject.js
@@ -0,0 +1,19 @@
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/learnyoumongo";
+var arg = Number(process.argv[2]);
+
+mongo.connect(url, function(err, db) {
+  if (err) {
+      db.close();
+      return console.log(err);
+  }
+  else {
+    var coll = db.collection('parrots') ;
+    coll.find({'age': {$gt: arg}}, {_id:0}).toArray(function(err, docs) {
+      if (err) return console.log(err);
+      console.log(docs);
+      db.close();
+    });
+  }
+})
+
